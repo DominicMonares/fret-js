977b062ca6004af9137dd54c22f8ae6246659595
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-18 23:27:03 -0800

    cleanup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -12,11 +12,14 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      record: true,
       start: false,
       currentNode: [],
       output: []
     };
     this.findFundamentalFreq = this.findFundamentalFreq.bind(this);
+    this.record = this.record.bind(this);
+    this.stopRecording = this.stopRecording.bind(this);
   }
 
   async setupContext() { // stream received
@@ -102,40 +105,50 @@ class App extends React.Component {
     return note[0];
   }
 
-  async detectPitch() {
-    var buffer = new Uint8Array(analyser.fftSize);
-    analyser.getByteTimeDomainData(buffer);
-    var fundamentalFreq = this.findFundamentalFreq(buffer, context.sampleRate);
-    if (fundamentalFreq !== -1) {
-      await this.setState({start: true})
-      var note = this.findClosestNote(fundamentalFreq, notesArray);
-      if (note.note !== 'F#8') {
-        var newCurrentNode = this.state.currentNode.slice();
-        newCurrentNode.push(note.note);
-        await this.setState({ currentNode: newCurrentNode });
+  async record(e) {
+    if (this.state.record) {
+      // detect pitch
+      var buffer = new Uint8Array(analyser.fftSize);
+      analyser.getByteTimeDomainData(buffer);
+      var fundamentalFreq = this.findFundamentalFreq(buffer, context.sampleRate);
+      if (fundamentalFreq !== -1) {
+        await this.setState({start: true})
+        var note = this.findClosestNote(fundamentalFreq, notesArray);
+        if (note.note !== 'F#8') {
+          var newCurrentNode = this.state.currentNode.slice();
+          newCurrentNode.push(note.note);
+          await this.setState({ currentNode: newCurrentNode });
+        }
+      } else if (this.state.start) {
+        var newOutput = this.state.output.slice();
+        var newNote = this.noteTally(this.state.currentNode.slice());
+        if (newNote !== '') {
+          newOutput.push(newNote);
+          await this.setState({ start: false, currentNode: [], output: newOutput });
+          console.log('OUTPUT ', this.state.output);
+        }
       }
-    } else if (this.state.start) {
-      var newOutput = this.state.output.slice();
-      var newNote = this.noteTally(this.state.currentNode.slice());
-      if (newNote !== '') {
-        newOutput.push(newNote);
-        await this.setState({ start: false, currentNode: [], output: newOutput });
-        console.log('OUTPUT ', this.state.output);
-      }
-    }
 
-    frameId = window.requestAnimationFrame(this.detectPitch.bind(this));
+      frameId = window.requestAnimationFrame(this.record.bind(this));
+    } else {
+      await this.setState({record: true});
+    }
+  }
+
+  stopRecording(e) {
+    this.setState({record: false});
   }
 
   componentDidMount() {
-    navigator.getUserMedia({audio: true}, this.setupContext.bind(this), (err) => {console.log(err)});
-    this.detectPitch();
+    navigator.getUserMedia({audio: true}, this.setupContext.bind(this), () => {console.log('CONTEXT SETUP FAILED')});
   }
 
   render() {
     return (
       <div>
         <h1>fretJs</h1>
+        <button onClick={this.record}>Start Recording</button>
+        <button onClick={this.stopRecording}>Stop Recording</button>
       </div>
     )
   }
