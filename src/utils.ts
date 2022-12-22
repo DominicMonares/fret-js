fee7b775965b11eeb3b1ece13220ca9591c6db2b
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-12-21 23:45:48 -0800

    refactor utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,33 +1,30 @@
+import notes from '../notes.json';
+
 export const findFundamentalFreq = (buffer: any, sampleRate: number) => { // TEMP ANY, MAKE TYPE
   const n = 1024
-  let bestR = 0
-  let bestK = -1;
+  let bestRate = 0
+  let bestKFrame = -1;
   for (let k = 8; k <= 6000; k++) {
     let sum = 0;
     for (let i = 0; i < n; i++) {
-      let signal1 = (buffer[i] - 127.5) / 127.5;
-      let signal2 = (buffer[i + k] - 127.5) / 127.5;
+      const signal1 = (buffer[i] - 127.5) / 127.5;
+      const signal2 = (buffer[i + k] - 127.5) / 127.5;
       sum += signal1 * signal2;
     }
 
     let r = sum / n;
-    if (r > bestR) {
-      bestR = r;
-      bestK = k;
+    if (r > bestRate) {
+      bestRate = r;
+      bestKFrame = k;
     }
 
     if (r > 0.9) break;
   }
 
-
-  if (bestR > 0.0025) {
-    return sampleRate / bestK;
-  } else {
-    return -1;
-  }
+  return bestRate > 0.0025 ? sampleRate / bestKFrame : -1;
 }
 
-export const findClosestNote = (freq: number, notes: any) => { // TEMP ANY, MAKE TYPE
+export const findClosestNote = (freq: number) => { // TEMP ANY, MAKE TYPE
   let low = -1
   let high = notes.length;
   while (high - low > 1) {
@@ -42,11 +39,28 @@ export const findClosestNote = (freq: number, notes: any) => { // TEMP ANY, MAKE
   return [notes[low], low];
 }
 
-const removeOvertones = (notes: any): string | number | null => { // TEMP ANY, MAKE TYPE
+export const noteWithinRange = (index: number) => index >= 25 && index <= 76 ? true : false;
+
+export const translateFreq = (shift: boolean, freq: any, batch: any) => { // TEMP ANY
+  const notePair = findClosestNote(freq);
+  const noteNode: any = notePair[0]; // TEMP ANY
+  const noteIndex = notePair[1];
+  if (noteWithinRange(noteIndex as number)) { // TEMP AS, FIGURE OUT WHAT'S GOING ON HERE
+    const newCurrentBatch = batch;
+    let noteKey;
+    !shift ? noteKey = noteNode['keys'][0] : noteKey = noteNode['keys'][1];
+    newCurrentBatch.push([noteKey, noteIndex]);
+    return newCurrentBatch;
+  }
+
+  return batch;
+}
+
+export const removeOvertones = (batch: any): string | number | null => { // TEMP ANY, MAKE TYPE
   let noteNode = [null, 0];
-  for (let i = 0; i < notes.length - 1; i++) {
-    if (noteNode[1] && notes[i][1] > noteNode[1]) {
-      noteNode = [notes[i][0], notes[i][1]];
+  for (let i = 0; i < batch.length; i++) {
+    if (noteNode[1] !== null && batch[i][1] > noteNode[1]) {
+      noteNode = [batch[i][0], batch[i][1]];
     }
   }
 
