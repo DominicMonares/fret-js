9bb9da7c30f647074a22bf02ffa0e98372adb5fe
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2023-01-08 18:52:37 -0800

    add comments

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -18,6 +18,7 @@ const App = () => {
   const [output, setOutput] = useState<string[]>([]);
   const [func, setFunc] = useState<string>('');
 
+  // Create audio context on page load
   useEffect(() => {
     navigator.getUserMedia(
       { audio: true },
@@ -26,27 +27,22 @@ const App = () => {
     );
   }, []);
 
-  useEffect(() => {
-    if (recording) {
-      startRecording([]);
-    } else if (!recording && recordingStarted) {
-      setRecordingStarted(false);
-      stopRecording();
-    }
-  });
-
   const setupContext = async () => {
     if (context.state === 'suspended') await context.resume();
-    const guitar = await navigator.mediaDevices.getUserMedia({
-      audio: {
-        echoCancellation: false,
-        autoGainControl: false,
-        noiseSuppression: false,
-        latency: 0
-      }
-    });
 
-    const source = context.createMediaStreamSource(guitar); // media stream audio source
+    // Remove mic optimizations - prevents distortion, warble, and other audio glitches
+    const audioSettings = {
+      echoCancellation: false,
+      autoGainControl: false,
+      noiseSuppression: false,
+      latency: 0
+    }
+
+    // Guitar stream - primary audio source
+    const guitar = await navigator.mediaDevices.getUserMedia({ audio: audioSettings });
+    const source = context.createMediaStreamSource(guitar);
+
+    // Prevent distortion loop
     const compressor = context.createDynamicsCompressor();
     compressor.threshold.setValueAtTime(-50, context.currentTime);
     compressor.knee.setValueAtTime(40, context.currentTime);
@@ -54,12 +50,78 @@ const App = () => {
     compressor.attack.setValueAtTime(0, context.currentTime);
     compressor.release.setValueAtTime(0.25, context.currentTime);
 
+    // Connect all nodes to destination
     source
       .connect(compressor)
       .connect(analyser)
       .connect(context.destination);
   }
 
+  // Control recording behavior
+  useEffect(() => {
+    if (recording) {
+      record([]);
+    } else if (!recording && recordingStarted) {
+      setRecordingStarted(false);
+      stopRecording();
+    }
+  });
+
+  const record = (batch: Batch, deadSignal?: number) => {
+    if (recording) {
+      // Detect pitch
+      const buffer = new Uint8Array(analyser.fftSize);
+      analyser.getByteTimeDomainData(buffer);
+      const fundamentalFreq = findFundamentalFreq(buffer, context.sampleRate);
+
+      // 59-1211 is freq range for B1-D6 (22 fret)
+      if (fundamentalFreq > 59 && fundamentalFreq < 1211) {
+        // Batch tracks every frequency recorded when a single note is played
+        const newBatch = batch.slice();
+        const char = translateFreq(shift, fundamentalFreq);
+        newBatch.push([char, fundamentalFreq]);
+        window.requestAnimationFrame(() => record(newBatch));
+      } else if (fundamentalFreq === -1) { // No frequency detected
+        // Prevent straggler frequencies, confirms note is finished
+        !deadSignal ? deadSignal = 1 : deadSignal++;
+
+        if (batch.length && deadSignal === 2) {
+          const char = removeOvertones(batch);
+          saveChar(char);
+        } else if (batch.length && deadSignal < 2) {
+          window.requestAnimationFrame(() => record(batch, deadSignal));
+        } else {
+          window.requestAnimationFrame(() => record([]));
+        }
+      } else {
+        window.requestAnimationFrame(() => record(batch));
+      }
+    }
+  }
+
+  const stopRecording = () => {
+    const newOutput = output.join('');
+    let newFunc;
+
+    // Execute code that the user wrote once recording is stopped
+    try {
+      newFunc = eval(`(${newOutput})`); // Never use eval on an app that needs security!
+    } catch (err: any) { // Can be any type of error
+      newFunc = err.toString();
+    }
+
+    setFunc(newFunc);
+  }
+
+  const clearRecording = () => {
+    setRecording(false);
+    setRecordingStarted(false);
+    setShift(false);
+    setOutput([]);
+    setFunc('');
+  }
+
+  // Add translated char to output tracker
   const saveChar = (char: string) => {
     const newOutput = output.slice();
     if (char !== '') {
@@ -75,54 +137,6 @@ const App = () => {
         setOutput(newOutput);
       }
     }
-  }
-
-  const startRecording = (batch: Batch, deadSignal?: number) => {
-    if (recording) {
-      // Detects pitch
-      const buffer = new Uint8Array(analyser.fftSize);
-      analyser.getByteTimeDomainData(buffer);
-      const fundamentalFreq = findFundamentalFreq(buffer, context.sampleRate);
-      // 59 is floor for B1 and 1211 is ceiling for D6
-      if (fundamentalFreq > 59 && fundamentalFreq < 1211) {
-        const newBatch = batch.slice();
-        newBatch.push([translateFreq(shift, fundamentalFreq), fundamentalFreq]);
-        window.requestAnimationFrame(() => startRecording(newBatch));
-      } else if (fundamentalFreq === -1) {
-        // Prevent straggler frequencies
-        !deadSignal ? deadSignal = 1 : deadSignal++;
-        if (batch.length && deadSignal === 2) {
-          saveChar(removeOvertones(batch));
-        } else if (batch.length && deadSignal < 2) {
-          window.requestAnimationFrame(() => startRecording(batch, deadSignal));
-        } else {
-          window.requestAnimationFrame(() => startRecording([]));
-        }
-      } else {
-        window.requestAnimationFrame(() => startRecording(batch));
-      }
-    }
-  }
-
-  const stopRecording = () => {
-    const newOutput = output.join('');
-    let newFunc;
-
-    try {
-      newFunc = eval('(' + newOutput + ')'); // Never use eval on an app that needs security!
-    } catch (err: any) { // Can be any type of error
-      newFunc = err.toString();
-    }
-
-    setFunc(newFunc);
-  }
-
-  const clearRecording = () => {
-    setRecording(false);
-    setRecordingStarted(false);
-    setShift(false);
-    setOutput([]);
-    setFunc('');
   }
 
   return (
