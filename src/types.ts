a0b284b920a1d8f44ca1e917a8f0a13cef47a3f7
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2023-01-08 20:04:58 -0800

    remove tone types

--- src/types.ts
+++ src/types.ts
@@ -2,20 +2,9 @@ export type BatchItem = [string, number];
 
 export type Batch = BatchItem[];
 
-export type MostTones = [string, number, number];
-
 export interface Note {
   note: string,
   frequency: number
 }
 
 export type Notes = Note[];
-
-export interface Tone {
-  count: number,
-  freq: number
-}
-
-export interface Tones {
-  [key: string]: Tone
-}
