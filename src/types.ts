a4882a17f89a1640401749d7716f1ed296a167e6
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2023-01-11 23:22:44 -0800

    add fret, note, and util types

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,26 @@
 export interface Chars {
-  [key: string]: number
+  [key: string]: number;
 }
 
+export type FindNote = (
+  targetFreq: number,
+  fretNum: FretNum
+) => NoteNode;
+
+export type FretNum = 22 | 24;
+
 export interface FretProps {
-  fretNum: 22 | 24
+  fretNum: FretNum;
 }
+
+export interface NoteNode {
+  note: string;
+  frequency: number;
+  chars: string[];
+}
+
+export type TranslateFreq = (
+  shift: boolean,
+  fundamentalFreq: number,
+  fretNum: FretNum
+) => string;
