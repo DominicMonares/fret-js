e487f187310bb34fca88ca9c2ae84ae28f95d2fc
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2023-01-12 23:57:45 -0800

    implement tune and fret nums

--- src/Components/Fretboard/Fretboard.tsx
+++ src/Components/Fretboard/Fretboard.tsx
@@ -14,19 +14,35 @@ const Fretboard = ({ fretNum, shift }: FretProps) => {
   });
 
   return (
-    <div className="fretboard-container">
-      <div className="fretboard">
-        {Object.values(diagram).flat().map(f => {
+    <div className="fb-diagram-container">
+      <div className="fb-tune">
+        {Object.keys(diagram).map(n => {
           return (
-            <div className="fret">
-              <span className="string"></span>
-              <span className="char">
-                {shift ? f.chars[1] : f.chars[0]}
-              </span>
-              <span className="string"></span>
-            </div>
+            <span className="fb-tune-note">{n}</span>
           );
         })}
+      </div>
+      <div className="fb-container">
+        <div className="fretboard">
+          {Object.values(diagram).flat().map(f => {
+            return (
+              <div className="fret">
+                <span className="string"></span>
+                <span className="char">
+                  {shift ? f.chars[1] : f.chars[0]}
+                </span>
+                <span className="string"></span>
+              </div>
+            );
+          })}
+        </div>
+        <div className="fret-nums">
+          {Array.from({ length: 25 }, (v, i) => i).map(n => {
+            return (
+              <span className="fret-num">{n}</span>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
