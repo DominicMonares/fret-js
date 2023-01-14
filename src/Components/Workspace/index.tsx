541db02bbd0ea281f6297441ab2f42785745b360
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2023-01-14 00:40:01 -0800

    styling

--- src/Components/Workspace/index.tsx
+++ src/Components/Workspace/index.tsx
@@ -5,12 +5,16 @@ const Workspace = ({ shift, input, output }: WorkspaceProps) => {
   return (
     <div className="workspace-container">
       <div className="console">
-        <span className="label">Input</span>
-        <span className="shift">{shift ? <div>SHIFT ON</div> : <></>}</span>
+        <div className="header">
+          <span className="label">Input</span>
+          <span className="shift">{shift ? <div>SHIFT ON</div> : <></>}</span>
+        </div>
         <div className="content">{input.join('')}</div>
       </div>
       <div className="console">
-        <span className="label">Output</span>
+        <div className="header">
+          <span className="label">Output</span>
+        </div>
         <div className="content">{output}</div>
       </div>
     </div>
